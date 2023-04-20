99417c7c513370c0d84631e313ee56c5414dc816
piet2b <piet2bezerra+dev@gmail.com>
2023-04-19 21:15:11 -0300

    vetores

---
+++ Vetores/Exercicio_05.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/*Faça um programa que calcule a soma dos vetores, elemento a elemento, em um terceiro vetor.
+O tamanho do vetor deverá ser 4.*/
+
+int main (){
+    
+    
+}
--- Vetores/Exercicio_06.c
+++ Vetores/Exercicio_06.c
@@ -20,7 +20,7 @@ int main (){
     printf("\nNúmeros múltiplos de 2: ");
     for(int i=0; i<7; i++){
         if(vetor[i] % 2 == 0){
-            printf("%d (posição %d) ", vetor[i], i);
+            printf("%d (posição %d) -", vetor[i], i+1);
             Mult2 = 1;
         }
     
@@ -31,10 +31,10 @@ int main (){
 
     // Verificando múltiplos de 3
     int Mult3 = 0;
-    printf("\nNúmeros múltiplos de 3: ");
+    printf("\nNúmeros múltiplos de 3: -");
     for(int i=0; i<7; i++){
         if(vetor[i] % 3 == 0){
-            printf("%d (posição %d) ", vetor[i], i);
+            printf("%d (posição %d) ", vetor[i], i+1);
             Mult3 = 1;
         }
     }
@@ -44,10 +44,10 @@ int main (){
 
     // Verificando múltiplos de 2 e de 3
     int Mult2e3 = 0;
-    printf("\nNúmeros múltiplos de 2 e de 3: ");
+    printf("\nNúmeros múltiplos de 2 e de 3: -");
     for(int i=0; i<7; i++){
         if(vetor[i] % 2 == 0 && vetor[i] % 3 == 0){
-            printf("%d (posição %d) ", vetor[i], i);
+            printf("%d (posição %d) ", vetor[i], i+1);
             Mult2e3 = 1;
         }
     }
