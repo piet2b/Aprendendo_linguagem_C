f0482d3a696412267ce054b26c396dab746b8122
piet2b <piet2bezerra+dev@gmail.com>
2023-04-24 15:00:43 -0300

    study

---
+++ Lista_01/Exercicio_20.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int num_sapatos, num_acima_39 = 0;
+    
+    do {
+        printf("Digite o tamanho do sapato (ou um número menor ou igual a 0 para sair): ");
+        scanf("%d", &num_sapatos);
+        
+        if (num_sapatos > 39) {
+            num_acima_39++;
+        }
+        
+    } while (num_sapatos > 0);
+    
+    printf("Foram digitados %d sapatos com tamanho acima de 39.\n", num_acima_39);
+    
+    return 0;
+}
