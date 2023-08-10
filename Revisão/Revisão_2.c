7346155b1e446619777ba099e5461606f5735074
piet2b <piet2bezerra+dev@gmail.com>
2023-08-10 19:11:57 -0300

    Exercicio 2

--- Revisão/Revisão_2.c
+++ Revisão/Revisão_2.c
@@ -3,11 +3,21 @@
 /*Calcule a soma dos N primeiros números inteiros positivos. O valor de N deve ser obtido
 através da entrada de dados*/
 
-int
+int main() {
+    int N;
+    int soma = 0;
 
-// Input 
-    printf("coloque 10 valores:\n");
-    for (int i = 0; i < 10; i++) {
-        scanf("%d", &valor[i]);
+    // Solicitar o valor de N ao usuário
+    printf("Digite um número inteiro positivo: ");
+    scanf("%d", &N);
+
+    // Calcular a soma dos primeiros N números inteiros positivos
+    for (int i = 1; i <= N; i++) {
+        soma += i;
     }
-    
+
+    // Exibir o resultado da soma
+    printf("A soma dos primeiros %d números inteiros positivos é: %d\n", N, soma);
+
+    return 0;
+}
